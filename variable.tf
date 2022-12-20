54ceb931fc5f62dc9ef33150230291fdfdb25130
Srinivasarao-M <Srinivasaraomaddila16@gmail.com>
2022-12-20 19:54:00 +0530

    eks

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "subnet_id_1" {
+    default = "subnet-05d1defd3f83a7669"
+}
+
+variable "subnet_id_2" {
+    default = "subnet-0e7573e3f83d99ece"
+}
